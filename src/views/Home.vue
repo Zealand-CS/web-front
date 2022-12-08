9e5088ea6b570b0a265f1fe9af62051c7961f25b
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2022-12-09 00:34:42 +0100

    filter-merge-homelist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
-<script setup lang="ts">
-import Header from '../components/Header.vue';
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
 import HomeList from '../components/HomeList.vue';
-import FilterComponent from '../components/FilterComponent.vue';
-import {mock} from '../api/users.api'
+import { mock, type UserAPI } from '@/api/users.api';
 
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HomeList,
+  },
+  setup() {
+    const users = ref<UserAPI[]>(mock);
 
+    const selectedFilter = ref<string | null>(null);
+    const searchQuery = ref('');
 
+    const filterCriteria = [
+      { id: 1, key: 'id', label: 'ID' },
+      { id: 2, key: 'email', label: 'Email' },
+      { id: 3, key: 'firstName', label: 'First Name' },
+      { id: 4, key: 'lastName', label: 'Last Name' }
+    ];
+
+    const filterData = (data: UserAPI[], selectedFilter: string | null, searchQuery: string): UserAPI[] => {
+      if (!selectedFilter) {
+        return data;
+      }
+
+      return data.filter(item => {
+        if (selectedFilter !== null) {
+          const filterValue = (item as any)[selectedFilter];
+          return filterValue.toString().toLowerCase().includes(searchQuery.toLowerCase());
+        }
+
+        return false;
+      });
+    };
+
+    const searchResults = computed(() => {
+      return filterData(mock, selectedFilter.value, searchQuery.value);
+    });
+
+    return { users, searchResults, selectedFilter, searchQuery, filterCriteria, filterData };
+  },
+});
 </script>
+
+
 <template>
   <div>
     <h1>Home</h1>
-    <!-- <HomeList :users="mock"/> -->
-    <FilterComponent :data="mock"></FilterComponent>
-    <HomeList :users="mock" />
-    <!-- <Header name="Adam" :age="96" /> -->
+
+    <label>Filter by:
+      <select v-model="selectedFilter">
+        <option value="null" selected>---</option>
+        <option v-for="criteria in filterCriteria" :value="criteria.key">
+          {{ criteria.label }}
+        </option>
+      </select>
+    </label>
+
+    <label>Search:
+      <input v-model="searchQuery" />
+    </label>
+
+    <HomeList :users="searchResults" />
   </div>
 </template>
+
